bb7da6522aad323429cacd57f66bbd7eecfdc668
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2020-10-08 14:25:28 +0200

    initial commit

---
+++ slagsmål/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace slagsmål
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
