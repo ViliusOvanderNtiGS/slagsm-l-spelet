c5e4728801d24b090abe6de2ef4b725c445d3210
Vilius <axelvilius.ovander@elev.ga.ntig.se>
2020-10-09 08:59:22 +0200

    cooling

--- slagsmål/Program.cs
+++ slagsmål/Program.cs
@@ -6,7 +6,58 @@ namespace slagsmål
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string play1 = "Lukas";
+            string play2 = "Vidar";
+            Console.WriteLine(play1 + " och " + play2 + " blir satans sura på varandra.");
+            Console.WriteLine("Ditt jobb är att titta på och trycka Enter några gånger.");
+            Console.WriteLine("Nu börjar skiten.");
+            Console.ReadLine();
+            int hp1 = 200;
+            int hp2 = 200;
+
+
+            Random generator = new Random();
+            Random secondGenerator = new Random();
+
+
+
+            while (hp1 >= 0 || hp2 >= 0)
+            {
+                int dmg1 = generator.Next(1, 31);
+                int dmg2 = secondGenerator.Next(1, 31);
+                hp1 = hp1 - dmg1;
+                hp2 = hp2 - dmg2;
+
+                Console.WriteLine(play1 + " har nu " + hp1 + " helsa kavar.");
+                Console.WriteLine(play2 + " har nu " + hp2 + " helsa kavar.");
+                Console.ReadLine();
+
+                // if (hp1 < 0 || hp2 < 0)
+                // {
+                //     hp1 = 0;
+                //     hp2 = 0;
+                // }
+            }
+
+            if (hp1 > hp2)
+            {
+                Console.WriteLine(play1 + " vann!");
+                Console.WriteLine(play2 + " luktar");
+            }
+            if (hp1 < hp2)
+            {
+                Console.WriteLine(play2 + " vann!");
+                Console.WriteLine(play1 + " luktar");
+            }
+            if (hp1 == hp2)
+            {
+                Console.WriteLine("Ingen vann");
+                Console.WriteLine("Båda luktar");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
